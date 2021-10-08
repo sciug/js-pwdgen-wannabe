220140469e8d0966b18e457fbb22377c2fe9a35e
sciug <gaetano.sciurti@libero.it>
2021-10-08 13:28:47 +0200

    start css style

--- java.js
+++ java.js
@@ -2,3 +2,8 @@ const userName = prompt("what's your name?")
 const userSureName = prompt("what's your surename?")
 const userAge = prompt("how old are you?")
 const color = prompt("what's your favourite colour?")
+
+
+
+document.getElementById("title").innerHTML = `Hi there ${userName}` 
+document.getElementById("infoUser").innerHTML = `Here's your info: ${userName} ${userSureName} ${userAge} ${color}`
