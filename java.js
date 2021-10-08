9ba3b0c3ac474b865c92940b608764de985fda7c
sciug <gaetano.sciurti@libero.it>
2021-10-08 13:28:06 +0200

    create html css js

---
+++ java.js
@@ -0,0 +1,4 @@
+const userName = prompt("what's your name?")
+const userSureName = prompt("what's your surename?")
+const userAge = prompt("how old are you?")
+const color = prompt("what's your favourite colour?")
